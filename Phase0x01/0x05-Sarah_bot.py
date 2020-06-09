65737e027407fbaecc92cdfe5322411411e21f02
johnconnor77 <jfcalleh@unal.edu.co>
2020-06-08 20:26:56 -0500

    Script with Sarah chatbot with best parsing of intents and entities format

---
+++ Phase0x01/0x05-Sarah_bot.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+#  ███████╗ █████╗ ██████╗  █████╗ ██╗  ██╗
+#  ██╔════╝██╔══██╗██╔══██╗██╔══██╗██║  ██║
+#  ███████╗███████║██████╔╝███████║███████║
+#  ╚════██║██╔══██║██╔══██╗██╔══██║██╔══██║
+#  ███████║██║  ██║██║  ██║██║  ██║██║  ██║
+#  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+#
+import os
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+import requests
+import time
+import json
+
+
+ENDPOINT = ""
+DEVICE_LABEL = ""
+VARIABLE_LABEL = ""
+TOKEN = ""
+
+
+def get_var(url=ENDPOINT, device=DEVICE_LABEL, variable=VARIABLE_LABEL,
+            token=TOKEN):
+    try:
+        url = "http://{}/api/v1.6/devices/{}/{}/lv".format(url, device, variable)
+
+        headers = {"X-Auth-Token": token, "Content-Type": "application/json"}
+
+        attempts = 0
+        status_code = 400
+
+        while status_code >= 400 and attempts < 5:
+            """print("[INFO] Retrieving data, attempt number: {}".format(attempts))"""
+            req = requests.get(url=url, headers=headers)
+            status_code = req.status_code
+            time.sleep(1)
+
+        """print("[INFO] Results:")
+        print(req.text)"""
+    except Exception as e:
+        print("[ERROR] Error posting, details: {}".format(e))
+
+    return req.text
+
+
+if __name__ == "__main__":
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'sarahdots-gtmiej-c64dd83054be.json'
+
+    DIALOGFLOW_PROJECT_ID = 'sarahdots-gtmiej'
+    DIALOGFLOW_LANGUAGE_CODE = 'en'
+    SESSION_ID = 'me'
+    ENDPOINT = "industrial.api.ubidots.com"
+    TOKEN = "BBFF-o9Wgd3hZokBMNd7d4vyzH9ZfLa5qj8"
+    OUT = ""
+
+    text_to_be_analyzed = "Can you tell me the last value of flow from River sensor?"
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
+
+    print(response)
+    print("Query text:", response.query_result.query_text)
+
+    DEVICE_LABEL = response.query_result.parameters['Device']
+    VARIABLE_LABEL = response.query_result.parameters['Variables']
+
+    OUT = get_var(ENDPOINT, DEVICE_LABEL, VARIABLE_LABEL, TOKEN)
+
+    print("Fulfillment text:", response.query_result.fulfillment_text + " " + OUT)
